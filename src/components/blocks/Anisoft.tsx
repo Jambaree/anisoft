42600a9f08448bf223d05a97bf74d3233c4e247e
Hamid Goudarzi <goudarzi.hamid@gmail.com>
2023-02-06 14:09:02 -0800

    add anisoft module

---
+++ src/components/blocks/Anisoft.tsx
@@ -0,0 +1,45 @@
+import Image from 'next/image';
+import Button from '../Button';
+import Edges from '../Edges';
+import Tilt from 'react-parallax-tilt';
+import FadeInUp from '../FadeInUp';
+
+function Anisoft({ data }) {
+	const { headline, tag, button, text } = data;
+
+	return (
+		<div className='primaryRadialBg pb-[100px] pl-[100px]'>
+			<div className='bg-white py-[100px] md:py-[150px]'>
+				<Edges size='lg'>
+					<div className='flex w-full h-full flex-wrap text-darkPurple items-center justify-between'>
+						<div className='max-w-[500px]'>
+							{headline && (
+								<FadeInUp className='delay-100'>
+									<h1 className='heroHeadline max-w-[985px] text-[32px]  sm:text-[48px]'>
+										{headline}
+									</h1>
+								</FadeInUp>
+							)}
+							{tag && <p className='max-w-[575px] pt-[40px]'>{tag}</p>}
+						</div>
+						<div className='max-w-[500px] flex flex-col'>
+							{text && <span>{text}</span>}
+							<div className='pt-[50px] flex wrap gap-[30px] flex-wrap-reverse w-auto mr-[50px]'>
+								{button?.url && (
+									<Button
+										variant='large'
+										href={button?.url}
+									>
+										{button?.title}
+									</Button>
+								)}
+							</div>
+						</div>
+					</div>
+				</Edges>
+			</div>
+		</div>
+	);
+}
+
+export default Anisoft;
