2fca73cc74256322fc033bb8f1ec1f87b4732e3b
Rene Sauve <rdasauve@gmail.com>
2023-02-06 12:22:48 -0800

    FooterTopperCTA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Mukta, Maven_Pro } from "@next/font/google";
 import "./globals.css";
 import Header from "../components/header";
 import Footer from "../components/footer";
+import FooterTopperCTA from "../components/FooterTopperCTA";
 
 const mukta = Mukta({
   variable: "--font-mukta",
@@ -27,6 +28,7 @@ export default function RootLayout({
         <Header />
 
         {children}
+        <FooterTopperCTA />
         <Footer />
       </body>
     </html>
