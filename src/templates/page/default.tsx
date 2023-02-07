09cf3e1f8ae6a70345db565b4259cad9b264a928
Rene Sauve <rdasauve@gmail.com>
2023-02-07 12:37:00 -0800

    LogoModule

--- src/templates/page/default.tsx
+++ src/templates/page/default.tsx
@@ -1,22 +1,24 @@
-import { notFound } from 'next/navigation';
-import Hero from '../../components/blocks/Hero';
-import data from '../../components/blocks/data.json';
-import { getData } from '@jambaree/next-wordpress';
-import Anisoft from '../../components/blocks/Anisoft';
+import { notFound } from "next/navigation";
+import Hero from "../../components/blocks/Hero";
+import data from "../../components/blocks/data.json";
+import { getData } from "@jambaree/next-wordpress";
+import Anisoft from "../../components/blocks/Anisoft";
+import LogoModule from "../../components/blocks/LogoModule";
 
 export default async function DefaultPageTemplate({ uri }) {
-   const { page } = await getData({ uri, query });
-	if (!page) {
-		notFound();
-	}
-	const { title } = page;
+  const { page } = await getData({ uri, query });
+  if (!page) {
+    notFound();
+  }
+  const { title } = page;
 
-	return (
-		<>
-			<Hero data={data.hero} />
+  return (
+    <>
+      <Hero data={data.hero} />
       <Anisoft data={data.anisoft} />
-		</>
-	);
+      <LogoModule data={data.logoModule} />
+    </>
+  );
 }
 const query = `
   query PageQuery($uri: ID!) {
