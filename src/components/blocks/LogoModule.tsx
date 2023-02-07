b4e66e38f8d14dd029f8c7208d5a5732362dd039
Rene Sauve <rdasauve@gmail.com>
2023-02-07 12:47:28 -0800

    added key to map

--- src/components/blocks/LogoModule.tsx
+++ src/components/blocks/LogoModule.tsx
@@ -8,10 +8,10 @@ const LogoModule = ({ data }) => {
       <div className="flex flex-col justify-center items-center py-[115px]">
         <h1 className="mb-[48px]">{data?.header}</h1>
         <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 grid-rows-2 gap-[24px] gap-y-[48px]">
-          {data?.logos?.map((logo) => {
-            console.log(logo);
+          {data?.logos?.map((logo, index) => {
             return (
               <Image
+                key={index}
                 src={logo?.url}
                 alt={logo?.alt}
                 width="195"
