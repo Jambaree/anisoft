0e5a04f3ccc3f825c6c5797e0c0333717e36edd2
Hamid Goudarzi <goudarzi.hamid@gmail.com>
2023-02-09 19:03:08 -0800

    add selecting function

--- src/components/MapBox.tsx
+++ src/components/MapBox.tsx
@@ -1,27 +1,37 @@
 'use client';
-import React from 'react';
+import React, { useState, useRef, useCallback } from 'react';
 // import ReactMapboxGl, { Marker, ZoomControl } from "react-mapbox-gl";
-import Map, { Marker, NavigationControl } from 'react-map-gl';
+import Map, { Marker, NavigationControl, MapRef } from 'react-map-gl';
 import MarkerIcon from '../../public/marker.svg';
 
 const Mapbox = (props: any) => {
 	const { locations } = props;
 
+	const mapRef = useRef<MapRef>();
+	const onSelectCity = useCallback((longitude, latitude) => {
+		mapRef.current?.flyTo({
+			center: [longitude, latitude],
+			zoom: 11,
+			duration: 2000,
+		});
+	}, []);
+
 	return (
 		<>
 			<Map
+				ref={mapRef}
 				initialViewState={{
 					latitude: 49.741015932602835,
 					longitude: -106.11014797030543,
 					zoom: 2,
 				}}
+				mapStyle='mapbox://styles/mapbox/light-v9'
 				style={{
 					maxHeight: '572px',
 					height: '100%',
 					width: '100%',
 					minHeight: '300px',
 				}}
-				mapStyle='mapbox://styles/mapbox/light-v10'
 				mapboxAccessToken={
 					'pk.eyJ1IjoiYmFyZWFkdmVydGlzaW5nIiwiYSI6ImNqaWRmdDMxZDAxdWw0MW9jYXRjYWRqZ3QifQ.DPR-gLvJOZzyf4X-Yk3V0g'
 				}
@@ -32,6 +42,9 @@ const Mapbox = (props: any) => {
 							key={index}
 							longitude={location.longitude}
 							latitude={location.latitude}
+							onClick={() => {
+								onSelectCity(location.longitude, location.latitude);
+							}}
 						>
 							<MarkerIcon />
 						</Marker>
