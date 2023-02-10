ae3b257d76651c230d6feb78a46af0a15cb7d7e7
Hamid Goudarzi <goudarzi.hamid@gmail.com>
2023-02-10 14:23:44 -0800

    wft, none of these packages does not work, red Friday

--- src/components/RichTextComponents.tsx
+++ src/components/RichTextComponents.tsx
@@ -1,14 +1,12 @@
 'use client';
 import React from 'react';
-import DOMPurify from 'dompurify';
 
 function RichTextComponents({ html }) {
-	const sanitizedHtml = DOMPurify.sanitize(html);
 
 	return (
 		<div
 			className='richText pt-[50px] md:pt-[70px]'
-			dangerouslySetInnerHTML={{ __html: sanitizedHtml }}
+			dangerouslySetInnerHTML={{ __html: html }}
 		/>
 	);
 }
