1ebedef253438c1b636686260227ed5f735fad84
Rene Sauve <rdasauve@gmail.com>
2023-02-13 10:51:57 -0800

    remove seo

--- src/app/[[...paths]]/head.tsx
+++ src/app/[[...paths]]/head.tsx
@@ -1,21 +1,21 @@
-import { YoastSEO, getYoastData, getSeedData } from "@jambaree/next-wordpress";
+// import { YoastSEO, getYoastData, getSeedData } from "@jambaree/next-wordpress";
 
 export default async function Head(props: { params: { paths: string[] } }) {
   const {
     params: { paths },
   } = props;
 
-  const uri = paths?.join?.("/") || "/";
-  const seedData = await getSeedData({ uri });
+  // const uri = paths?.join?.("/") || "/";
+  // const seedData = await getSeedData({ uri });
 
-  const yoastData = await getYoastData({
-    uri,
-    name: seedData?.name || seedData?.contentTypeName || "page",
-  });
+  // const yoastData = await getYoastData({
+  //   uri,
+  //   name: seedData?.name || seedData?.contentTypeName || "page",
+  // });
 
   return (
     <head>
-      <YoastSEO ogUrl={uri} seo={yoastData?.seo} />
+      {/* <YoastSEO ogUrl={uri} seo={yoastData?.seo} /> */}
       <link
         href="https://api.mapbox.com/mapbox-gl-js/v0.51.0/mapbox-gl.css"
         rel="stylesheet"
