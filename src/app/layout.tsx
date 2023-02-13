9697bd527bf8525c396b81bd08a14eea7afa1e8f
Rene Sauve <rdasauve@gmail.com>
2023-02-13 12:39:23 -0800

    move footer query to footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import "./globals.css";
 import Header from "../components/header";
 import Footer from "../components/footer";
 import { getData } from "@jambaree/next-wordpress";
-import useMenuItems from "../components/header/headerMenu/useMenuItems";
+import useMenuItems from "../components/useMenuItems";
 
 const mukta = Mukta({
   variable: "--font-mukta",
@@ -33,33 +33,6 @@ export default async function RootLayout({
   const headerMenuItems = await useMenuItems({
     name: "header",
   });
-  // const footerMenuItems = await useMenuItems({
-  //   name: "footer",
-  // });
-
-  // const productMenuItems = await getMenuItems({
-  //   location: "PRODUCT_FOOTER_MENU",
-  //   slug: "product-footer-menu",
-  // });
-  // const footerMenuItems = await getMenuItems({
-  //   location: "FOOTER_MENU",
-  //   slug: "footer-menu",
-  // });
-
-  const footerMenuItems = [
-    {
-      id: "1",
-      label: "About",
-      url: "/about",
-      childItems: [
-        {
-          id: "2",
-          label: "About",
-          url: "/about",
-        },
-      ],
-    },
-  ];
 
   return (
     <html lang="en" className={`${maven.variable} ${mukta.variable}`}>
@@ -67,10 +40,10 @@ export default async function RootLayout({
         <Header data={header} menuItems={headerMenuItems} />
 
         {children}
-
+        {/* @ts-expect-error Server Component */}
         <Footer
           data={footer}
-          menuItems={footerMenuItems}
+
           // productMenuItems={productMenuItems}
         />
       </body>
