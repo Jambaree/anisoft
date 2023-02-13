723dd157132d7c70d36ea5fe352ca4d9a65b9368
Rene Sauve <rdasauve@gmail.com>
2023-02-13 14:09:35 -0800

    removed head and replaced with new nextjs generateMetaData ~ updated Jambaree package

--- src/app/[[...paths]]/page.tsx
+++ src/app/[[...paths]]/page.tsx
@@ -1,4 +1,9 @@
-import { WordpressTemplate } from "@jambaree/next-wordpress";
+import {
+  WordpressTemplate,
+  getSeedData,
+  getYoastData,
+} from "@jambaree/next-wordpress";
+import type { Metadata } from "next";
 import templates from "../../templates";
 
 export default async function PageTemplate(props: {
@@ -8,5 +13,50 @@ export default async function PageTemplate(props: {
     params: { paths },
   } = props;
 
-  return <WordpressTemplate paths={paths} templates={templates} />;
+  return (
+    <>
+      {/* THIS IS BEING USED - current issue with app directory*/}
+      {/* @ts-expect-error Server Component */}
+      <WordpressTemplate paths={paths} templates={templates} />{" "}
+    </>
+  );
+}
+
+export async function generateMetadata({ params }): Promise<Metadata> {
+  const uri = params?.paths?.join?.("/") || "/";
+  const seedData = await getSeedData({ uri });
+
+  // const uri = paths?.join?.("/") || "/";
+  // const seedData = await getSeedData({ uri });
+
+  const yoastData = await getYoastData({
+    uri,
+    name: seedData?.name || seedData?.contentTypeName || "page",
+  });
+
+  return {
+    title: yoastData?.seo?.title,
+    description: yoastData?.seo?.metaDesc,
+    openGraph: {
+      title: yoastData?.seo?.opengraphTitle,
+      description: yoastData?.seo?.opengraphDescription,
+      url: yoastData?.seo?.opengraph?.url,
+      siteName: yoastData?.seo?.opengraphDescription,
+      // images: [
+      //   {
+      //     url: 'https://nextjs.org/og.png',
+      //     width: 800,
+      //     height: 600,
+      //   },
+      //   {
+      //     url: 'https://nextjs.org/og-alt.png',
+      //     width: 1800,
+      //     height: 1600,
+      //     alt: 'My custom alt',
+      //   },
+      // ],
+      locale: "en-US",
+      type: "website",
+    },
+  };
 }
