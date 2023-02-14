582976b1efa44bdf7d826385ae72ae02027eba71
Rene Sauve <rdasauve@gmail.com>
2023-02-14 15:58:37 -0800

    Update CardsNavBar.tsx

--- src/components/servicecards/CardsNavBar.tsx
+++ src/components/servicecards/CardsNavBar.tsx
@@ -5,7 +5,7 @@ import { motion, AnimatePresence } from "framer-motion";
 
 import ChevronUp from "../../../public/chevron-up.svg";
 
-const CardsNavBar = ({ services, activeIndex, handleScrollToTop }) => {
+const CardsNavBar = ({ services, activeIndex }) => {
   const refArray = useRef([]);
   const [activeNav, setActiveNav] = useState(-1);
 
