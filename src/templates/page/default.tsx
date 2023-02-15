e475b8a97490b32e42fc9f4f76c9eb511c19f68c
Caleb Barnes <caleb.d.barnes@gmail.com>
2023-02-15 14:45:26 -0800

    add GraphQL comment for page query

--- src/templates/page/default.tsx
+++ src/templates/page/default.tsx
@@ -25,7 +25,7 @@ export default async function DefaultPageTemplate({ uri }) {
     </div>
   );
 }
-const query = `
+const query = /* GraphQL */ `
   query PageQuery($uri: ID!) {
     page(id: $uri, idType: URI) {
       __typename
@@ -33,137 +33,137 @@ const query = `
       title
       uri
       slug
-	  template {
-
-		templateName
-		... on DefaultTemplate {	
-			footerTopperCta {
-				button {
-				  title
-				  url
-				}
-				text1
-				text2
-			  }
-		  templateName
-		  acf {
-			modules {
-				__typename
-			  ... on DefaultTemplate_Acf_Modules_Anisoft {
-				fieldGroupName
-				headline
-				tag
-				text
-				button {
-				  title
-				  url
-				}
-			  }
-			  ... on DefaultTemplate_Acf_Modules_Hero {
-				fieldGroupName
-				headline
-				button2 {
-				  title
-				  url
-				}
-				button1 {
-				  title
-				  url
-				}
-				image {
-				  sourceUrl
-				  altText
-				}
-				subHeadline
-			  }
-			  ... on DefaultTemplate_Acf_Modules_InfoCallout {
-				fieldGroupName
-				headline
-				tag
-				text
-				button2 {
-				  title
-				  url
-				}
-				button1 {
-				  url
-				  title
-				}
-			  }
-			  ... on DefaultTemplate_Acf_Modules_Logos {
-				fieldGroupName
-				header
-				logos {
-				  logo {
-					sourceUrl
-					altText
-				  }
-				}
-			  }
-			  ... on DefaultTemplate_Acf_Modules_ProductsSlider {
-				fieldGroupName
-				headline
-				products {
-				  text
-				  buttonText
-				  product {
-					... on Post {
-					  id
-					  uri
-					  title
-					  featuredImage {
-						node {
-						  sourceUrl
-						  altText
-						}
-					  }
-					}
-				  }
-				}
-				tag
-			  }
-			  ... on DefaultTemplate_Acf_Modules_Stats {
-				description
-				fieldGroupName
-				image {
-					sourceUrl
-				}
-				stats {
-				  stat
-				  label
-				}
-				title
-			  }
-			  ... on DefaultTemplate_Acf_Modules_TextInfo {
-				tag
-				headline
-				button1 {
-					title
-					url
-					target
-				}
-				button2 {
-					title
-					url
-					target
-				}
-				text
-			  }
-			  ... on DefaultTemplate_Acf_Modules_Testimonials {
-				fieldGroupName
-				testimonials {
-				  description
-				  name
-				  title
-				  image {
-					sourceUrl
-					altText
-				  }
-				}
-			  }
-			}
-		  }
-		}
-	  }
+      template {
+        templateName
+        ... on DefaultTemplate {
+          footerTopperCta {
+            button {
+              title
+              url
+            }
+            text1
+            text2
+          }
+          templateName
+          acf {
+            modules {
+              __typename
+              ... on DefaultTemplate_Acf_Modules_Anisoft {
+                fieldGroupName
+                headline
+                tag
+                text
+                button {
+                  title
+                  url
+                }
+              }
+              ... on DefaultTemplate_Acf_Modules_Hero {
+                fieldGroupName
+                headline
+                button2 {
+                  title
+                  url
+                }
+                button1 {
+                  title
+                  url
+                }
+                image {
+                  sourceUrl
+                  altText
+                }
+                subHeadline
+              }
+              ... on DefaultTemplate_Acf_Modules_InfoCallout {
+                fieldGroupName
+                headline
+                tag
+                text
+                button2 {
+                  title
+                  url
+                }
+                button1 {
+                  url
+                  title
+                }
+              }
+              ... on DefaultTemplate_Acf_Modules_Logos {
+                fieldGroupName
+                header
+                logos {
+                  logo {
+                    sourceUrl
+                    altText
+                  }
+                }
+              }
+              ... on DefaultTemplate_Acf_Modules_ProductsSlider {
+                fieldGroupName
+                headline
+                products {
+                  text
+                  buttonText
+                  product {
+                    ... on Post {
+                      id
+                      uri
+                      title
+                      featuredImage {
+                        node {
+                          sourceUrl
+                          altText
+                        }
+                      }
+                    }
+                  }
+                }
+                tag
+              }
+              ... on DefaultTemplate_Acf_Modules_Stats {
+                description
+                fieldGroupName
+                image {
+                  sourceUrl
+                }
+                stats {
+                  stat
+                  label
+                }
+                title
+              }
+              ... on DefaultTemplate_Acf_Modules_TextInfo {
+                tag
+                headline
+                button1 {
+                  title
+                  url
+                  target
+                }
+                button2 {
+                  title
+                  url
+                  target
+                }
+                text
+              }
+              ... on DefaultTemplate_Acf_Modules_Testimonials {
+                fieldGroupName
+                testimonials {
+                  description
+                  name
+                  title
+                  image {
+                    sourceUrl
+                    altText
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
     }
-  }`;
+  }
+`;
