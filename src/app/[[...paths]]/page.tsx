14395df5b2a5a251d0b9525e15eb8641d433a652
Rene Sauve <rdasauve@gmail.com>
2023-02-14 17:30:04 -0800

    product footer

--- src/app/[[...paths]]/page.tsx
+++ src/app/[[...paths]]/page.tsx
@@ -29,10 +29,10 @@ export async function generateStaticParams() {
   const nodePaths = await getAllContentNodePaths();
 
   return nodePaths.map((node) => {
-    const beginningUri = node.uri.split("/").slice(1);
+    const seperatedUrls = node.uri.split("/").slice(1);
 
     return {
-      paths: [...(beginningUri || "/")],
+      paths: [...(seperatedUrls || "/")],
     };
   });
 }
