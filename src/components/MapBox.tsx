92494664ba648269788423bdecba56b63d987dc8
Hamid Goudarzi <goudarzi.hamid@gmail.com>
2023-03-02 08:28:14 -0800

    Update MapBox.tsx

--- src/components/MapBox.tsx
+++ src/components/MapBox.tsx
@@ -5,40 +5,40 @@ const Mapbox = (props: any) => {
 	const { locations, onSelectCity, mapRef } = props;
 
 	return (
-		<>
-			<Map
-				ref={mapRef}
-				initialViewState={{
-					latitude: 49.741015932602835,
-					longitude: -106.11014797030543,
-					zoom: 2,
-				}}
-				mapStyle='mapbox://styles/mapbox/light-v9'
-				style={{
-					height: '100%',
-					width: '100%',
-					minHeight: '300px',
-				}}
-				mapboxAccessToken={process.env.NEXT_MAPBOX}
-			>
-				{locations.length > 0 &&
-					locations.map((location, index) => (
-						<Marker
-							key={index}
-							longitude={location.longitude}
-							latitude={location.latitude}
-							onClick={() => {
-								onSelectCity(location.longitude, location.latitude, 11, index);
-							}}
-						>
-							<MarkerIcon className='translate-y-[-25px]' />
-						</Marker>
-					))}
+    <>
+      <Map
+        ref={mapRef}
+        initialViewState={{
+          latitude: 56.1304,
+          longitude: -106.3468,
+          zoom: 3,
+        }}
+        mapStyle="mapbox://styles/mapbox/light-v9"
+        style={{
+          height: "100%",
+          width: "100%",
+          minHeight: "300px",
+        }}
+        mapboxAccessToken={process.env.NEXT_MAPBOX}
+      >
+        {locations.length > 0 &&
+          locations.map((location, index) => (
+            <Marker
+              key={index}
+              longitude={location.longitude}
+              latitude={location.latitude}
+              onClick={() => {
+                onSelectCity(location.longitude, location.latitude, 11, index);
+              }}
+            >
+              <MarkerIcon className="translate-y-[-25px]" />
+            </Marker>
+          ))}
 
-				<NavigationControl position={'top-right'} />
-			</Map>
-		</>
-	);
+        <NavigationControl position={"top-right"} />
+      </Map>
+    </>
+  );
 };
 
 export default Mapbox;
