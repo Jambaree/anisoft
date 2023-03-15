19e561b58c10b3b91f01046921523a69528443c2
Hamid Goudarzi <goudarzi.hamid@gmail.com>
2023-03-15 15:56:58 -0700

    add space btw title and brd

--- src/components/PageHeader.tsx
+++ src/components/PageHeader.tsx
@@ -10,13 +10,13 @@ const PageHeader = ({ data }) => {
     <div className=" py-[90px] overflow-x-hidden">
       <Edges size="lg">
         <div className="flex flex-wrap md:justify-between">
-          <SideIn right={false} className="flex flex-col md:w-[30%]">
+          <SideIn right={false} className="flex flex-col md:w-[30%] ">
             <BreadCrumbs />
             {title && (
               <h1 className="heroHeadline mb-[30px] md:mb-0">{title}</h1>
             )}
           </SideIn>
-          <SideIn className="md:w-[70%] py-2">
+          <SideIn className="md:w-[65%] py-2">
             {content && <div>{parse(content)}</div>}
           </SideIn>
         </div>
