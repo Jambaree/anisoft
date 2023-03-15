64edb9b5b3a5e4529d1c9ef1278ced24e034d796
Hamid Goudarzi <goudarzi.hamid@gmail.com>
2023-03-15 11:23:02 -0700

    adjust function

--- src/utils/getUrlPath.ts
+++ src/utils/getUrlPath.ts
@@ -1,6 +1,35 @@
 import parsePath from "parse-path";
 
+ const normalizePath = (path) => {
+  if (path.endsWith(`/`)) {
+    path = path.slice(0, -1);
+  }
+
+  if (!path.startsWith(`/`)) {
+    path = `/${path}`;
+  }
+
+  path = decodeURI(path);
+
+  // Replace Backend URL
+  path = path.replace(`${process.env.NEXT_PUBLIC_WP_URL}/`, "");
+
+  // Remove trailing slash
+  if (path.substr(-1) === "/" && path.length > 1) {
+    path = path.substr(0, path.length - 1);
+  }
+
+  return path;
+};
+
+/**
+ * This is temporary until we can get a path field from MenuItems https://github.com/wp-graphql/wp-graphql/issues/1137
+ *
+ * https://stackoverflow.com/questions/736513/how-do-i-parse-a-url-into-hostname-and-path-in-javascript
+ */
 export const getUrlPath = (url) => {
   const parsedUrl = parsePath(url);
-  return parsedUrl.pathname;
+  console.log(parsedUrl);
+
+  return normalizePath(parsedUrl.href);
 };
