4d946ec7b8164bd31fa2b056e0af52c829289bc9
Rene Sauve <rdasauve@gmail.com>
2023-04-12 11:30:00 -0700

    add revalidate

--- src/pages/api/revalidate.ts
+++ src/pages/api/revalidate.ts
@@ -1,43 +1,48 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
-type Data = {
-  status: "success" | "error";
-  message?: string;
-  path?: string[];
-};
-
-export default async function Revalidation(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  const path: string[] = req.body.path;
-  console.log(`Revalidating: ${path}`);
-  // todo: add secret to prevent unauthorized requestset to confirm this is a valid request
-  //   if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
-  //     return res.status(401).json({ message: "Invalid token" });
-  //   }
+  const {
+    body: { paths, postId },
+    method,
+  } = req;
 
-  if (!path || !path.length) {
-    return res.status(400).json({
-      status: "error",
-      message: "No path provided",
-    });
+  console.log(req.body);
+  // if (
+  //   req.headers.authorization !== `Bearer ${process.env.REVALIDATE_SECRET_KEY}`
+  // ) {
+  //   return res.status(401).json({ message: "Invalid token" });
+  // }
+
+  if (method !== "PUT") {
+    return res.status(405).json({ message: `Method ${method} Not Allowed` });
   }
 
+  if (!paths) {
+    return res.status(412).json({ message: "No paths" });
+  }
+
+  const correctPaths = paths.filter((path: string) => path.startsWith("/"));
+
   try {
-    await res.revalidate(`/${path}`);
+    const revalidatePaths = correctPaths.map((path: string) =>
+      res.revalidate(path, { unstable_onlyGenerated: false })
+    );
+
+    await Promise.all(revalidatePaths);
+
+    // Logging for debugging purposes only
+    console.log(
+      `${new Date().toJSON()} - Paths revalidated: ${correctPaths.join(", ")}`
+    );
 
     return res.json({
-      status: "success",
-      message: `Revalidated ${path.length} path(s)`,
-      path,
+      revalidated: true,
+      message: `Paths revalidated: ${correctPaths.join(", ")}`,
     });
   } catch (err) {
-    // If there was an error, Next.js will continue
-    // to show the last successfully generated page
-    return res.status(500).send({
-      status: "error",
-      message: `Internal server error`,
-    });
+    return res.status(500).json({ message: err.message });
   }
 }
