e46149accad4dde9fa54916e97f442dc10e97dfd
Hamid Goudarzi <goudarzi.hamid@gmail.com>
2023-05-25 13:38:07 -0700

    Update MobileSubMenu.tsx

--- src/components/header/headerMenu/MobileSubMenu.tsx
+++ src/components/header/headerMenu/MobileSubMenu.tsx
@@ -57,7 +57,7 @@ const MobileSubMenu = ({
             initial="closed"
             animate="open"
             variants={sideVariants}
-            className="fixed z-50 overflow-y-scroll mobileMenu pb-[300px] min-h-fit"
+            className="fixed z-50 overflow-y-scroll mobileMenu pb-[300px] h-full"
           >
             <motion.div variants={itemVariants} className="">
               {menu?.nodes?.map((item, index) => (
