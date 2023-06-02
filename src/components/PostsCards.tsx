73403d81abc0a216a26c16ac2353e0081996cd4a
Hamid Goudarzi <goudarzi.hamid@gmail.com>
2023-06-02 13:41:09 -0700

    add new module

---
+++ src/components/PostsCards.tsx
@@ -0,0 +1,22 @@
+
+// import React from "react";
+// import { getData } from "@jambaree/next-wordpress";
+
+// export default async function PostsCards({ categoryName }) {
+//   const { posts } = await getData({
+//     query,
+//   });
+
+//   return <div></div>;
+// }
+
+// const query = /* GraphQL */ `
+//   query ServicePageQuery {
+//     posts {
+//       nodes {
+//         id
+//         title
+//       }
+//     }
+//   }
+// `;
