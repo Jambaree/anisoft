da9970a3ca6b4321293c88bf8d9d6584bb7dc276
Hamid Goudarzi <goudarzi.hamid@gmail.com>
2023-06-09 13:25:34 -0700

    using GoogleTagManager instead script directly to increase speed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import Header from "../components/header";
 import Footer from "../components/footer";
 import { getData } from "@jambaree/next-wordpress";
 import Providers from "../components/Providers";
+import GoogleTagManager from "../components/GoogleTagManager";
 
 const mukta = Mukta({
   variable: "--font-mukta",
@@ -43,7 +44,7 @@ export default async function RootLayout({
           href="https://api.mapbox.com/mapbox-gl-js/v0.51.0/mapbox-gl.css"
           rel="stylesheet"
         />
-        <script
+        {/* <script
           async
           id="google-tag-manager"
           strategy="afterInteractive"
@@ -54,7 +55,8 @@ export default async function RootLayout({
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-5NP3J68');`,
           }}
-        />
+        /> */}
+        <GoogleTagManager />
       </head>
       <body id="top">
         <noscript
