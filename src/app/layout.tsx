d648ebea7892c8fedd212c0458477c2b07c1d064
Hamid Goudarzi <51421954+hamid-goudarzi@users.noreply.github.com>
2023-07-26 08:24:38 -0700

    Improve performance (#51)
    * add script component
    
    * add partytown
    
    * add partytown to next script
    
    * add partytown to npm before build
    
    * Update layout.tsx
    
    * Update Scripts.tsx
    
    * Update Scripts.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import Header from "../components/header";
 import Footer from "../components/footer";
 import { getData } from "@jambaree/next-wordpress";
 import Providers from "../components/Providers";
+import { Partytown } from "@builder.io/partytown/react";
+import Scripts from "../components/Scripts";
 
 const mukta = Mukta({
   variable: "--font-mukta",
@@ -43,7 +45,10 @@ export default async function RootLayout({
           href="https://api.mapbox.com/mapbox-gl-js/v0.51.0/mapbox-gl.css"
           rel="stylesheet"
         />
+        <Scripts />
+        {/* <Partytown debug={true} forward={["dataLayer.push"]} />
         <script
+          type="text/partytown"
           async
           id="google-tag-manager"
           strategy="afterInteractive"
@@ -54,14 +59,14 @@ export default async function RootLayout({
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','GTM-5NP3J68');`,
           }}
-        />
+        /> */}
       </head>
       <body id="top">
-        <noscript
+        {/* <noscript
           dangerouslySetInnerHTML={{
             __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5NP3J68" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
           }}
-        />
+        /> */}
         <Header data={header} menuItems={headerMenuItems} />
         <Providers>{children}</Providers>
         {/* @ts-expect-error Server Component */}
