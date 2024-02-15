72f508a51544c715e19bdcf3accca92a1c0b1f9c
Caleb Barnes <caleb.d.barnes@gmail.com>
2024-02-15 14:20:01 -0800

    adding pagination

--- src/components/blog/blog-pagination.tsx
+++ src/components/blog/blog-pagination.tsx
@@ -23,79 +23,97 @@ export function BlogPagination({
   totalPages: number;
   basePath?: string;
 }) {
-  const siblingCount = 1; // Adjust this to show more pages around the current page
-  const firstPageIndex = 1;
-  const lastPageIndex = totalPages;
-
   const range = (start, end) => {
     const length = end - start + 1;
     return Array.from({ length }, (_, idx) => idx + start);
   };
 
-  const paginationRange = (() => {
-    const totalPageNumbers = siblingCount + 5; // siblings + firstPage + lastPage + currentPage + 2 ellipses
-
-    if (totalPages > totalPageNumbers) {
-      const startPage = Math.max(2, currentPage - siblingCount);
-      const endPage = Math.min(totalPages - 1, currentPage + siblingCount);
-      let pages = range(startPage, endPage);
-
-      const hasLeftSpill = startPage > 2;
-      const hasRightSpill = totalPages - endPage > 1;
-      const spillOffset = totalPageNumbers - (pages.length + 1);
-
-      if (hasLeftSpill && !hasRightSpill) {
-        const extraPages = range(startPage - spillOffset, startPage - 1);
-        pages = [firstPageIndex, ...extraPages, ...pages];
-      } else if (!hasLeftSpill && hasRightSpill) {
-        const extraPages = range(endPage + 1, endPage + spillOffset);
-        pages = [...pages, ...extraPages, lastPageIndex];
-      } else {
-        pages = [firstPageIndex, ...pages, lastPageIndex];
-      }
-
-      return pages;
+  const makePaginationRange = () => {
+    // Always include the first and last pages if totalPages is more than 1
+    if (totalPages <= 1) {
+      return range(1, totalPages);
     }
 
-    return range(1, totalPages);
-  })();
+    const paginationRange = [];
+
+    // Define the range of pages to show around the current page
+    const startPage = Math.max(2, currentPage - 1);
+    const endPage = Math.min(totalPages - 1, currentPage + 1);
+
+    // Always include the first page
+    paginationRange.push(1);
+
+    // If the current page is more than 2 steps away from the first page, we add ellipsis
+    if (currentPage > 3) {
+      paginationRange.push("...");
+    }
+
+    // Range of pages around the current page
+    if (totalPages > 1) {
+      for (let page = startPage; page <= endPage; page++) {
+        if (page !== 1 && page !== totalPages) {
+          paginationRange.push(page);
+        }
+      }
+    }
+
+    // If the current page is more than 2 steps away from the last page, we add ellipsis
+    if (currentPage < totalPages - 2) {
+      paginationRange.push("...");
+    }
+
+    // Always include the last page if totalPages is more than 1
+    if (totalPages > 1) {
+      paginationRange.push(totalPages);
+    }
+
+    return paginationRange;
+  };
+
+  const paginationRange = makePaginationRange();
 
   return (
     <Pagination className={className}>
       <PaginationContent>
-        {/* Previous Page */}
-        <PaginationItem>
-          <PaginationPrevious disabled={currentPage === 1} href={prevPage} />
-        </PaginationItem>
+        {prevPage ? (
+          <PaginationItem>
+            <PaginationPrevious href={prevPage} />
+          </PaginationItem>
+        ) : null}
 
-        {/* Dynamic Pagination */}
-        {paginationRange.map((page, index) =>
-          page === currentPage ? (
+        {paginationRange.map((page, index) => {
+          if (page === "...") {
+            return (
+              <PaginationItem key={index}>
+                <PaginationEllipsis />
+              </PaginationItem>
+            );
+          } else if (page === 1) {
+            return (
+              <PaginationItem key={index}>
+                <PaginationLink href={basePath} isActive={currentPage === page}>
+                  {page}
+                </PaginationLink>
+              </PaginationItem>
+            );
+          }
+          return (
             <PaginationItem key={index}>
-              <PaginationLink aria-current="page" href="#">
+              <PaginationLink
+                href={`${basePath}/${page}`}
+                isActive={currentPage === page}
+              >
                 {page}
               </PaginationLink>
             </PaginationItem>
-          ) : page === 0 ? (
-            <PaginationItem key={index}>
-              <PaginationEllipsis />
-            </PaginationItem>
-          ) : (
-            <PaginationItem key={index}>
-              <PaginationLink href={`${basePath}/${page}`}>
-                {page}
-              </PaginationLink>
-            </PaginationItem>
-          )
-        )}
+          );
+        })}
 
-        {/* Next Page */}
-        <PaginationItem>
-          <PaginationNext
-            disabled={currentPage === totalPages}
-            href={nextPage}
-          />
-        </PaginationItem>
+        {nextPage ? (
+          <PaginationItem>
+            <PaginationNext href={nextPage} />
+          </PaginationItem>
+        ) : null}
       </PaginationContent>
     </Pagination>
   );
