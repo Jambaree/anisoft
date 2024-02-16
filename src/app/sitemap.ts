c0db26fa475d95f5ac673ed709ef27e4fd5d3df0
Caleb Barnes <caleb.d.barnes@gmail.com>
2024-02-16 13:27:37 -0800

    fix sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,1 +1,7 @@
-export { generateSitemap as default } from "@nextwp/core";
+import { generateSitemap } from "@nextwp/core";
+
+export default function sitemap() {
+  return generateSitemap({
+    postTypes: ["pages", "posts", "solutions-products"],
+  });
+}
