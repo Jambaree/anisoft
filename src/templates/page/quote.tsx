e5a29f9cafc43b4ad851c3a42ce28c08d6372ddf
Caleb Barnes <caleb.d.barnes@gmail.com>
2024-02-27 13:12:10 -0800

    fix form submit / button type

--- src/templates/page/quote.tsx
+++ src/templates/page/quote.tsx
@@ -3,16 +3,14 @@ import GravityForm from "../../components/form/GravityForm";
 import PageHeader2 from "../../components/PageHeader2";
 
 export default function QuotePageTemplate({ data }: { data: WpPage }) {
-  const {
-    title: { rendered: title },
-    content: { rendered: content },
-    acf,
-  } = data;
+  const { title, content, acf } = data;
 
   return (
     <div>
-      <PageHeader2 text={content} title={title} />
-      <GravityForm formId={acf?.form?.form_id} />
+      <PageHeader2 text={content?.rendered} title={title?.rendered} />
+      <div className="my-[65px]">
+        <GravityForm formId={acf?.form?.form_id} />
+      </div>
     </div>
   );
 }
