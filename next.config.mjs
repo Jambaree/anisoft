4e7f5bd0d6195ce11531a885d69a7bcb6532e8c8
renesauve <rdasauve@gmail.com>
2024-04-02 15:38:27 -0700

    next-config content-security-policy headers

--- next.config.mjs
+++ next.config.mjs
@@ -47,11 +47,11 @@ const nextConfig = {
       {
         source: "/:path*",
         headers: [
-          // {
-          //   key: "Content-Security-Policy",
-          //   value:
-          //     "default-src 'self'; script-src 'self'; img-src 'self' data:; style-src 'self';",
-          // },
+          {
+            key: "Content-Security-Policy",
+            value:
+              "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://www.googletagmanager.com https://googleads.g.doubleclick.net https://www.google-analytics.com; style-src 'self' 'unsafe-inline' https://api.mapbox.com; img-src 'self' data:; media-src 'self' https://anisoft.wpengine.com; connect-src 'self' https://www.google-analytics.com; frame-src 'self' https://td.doubleclick.net;",
+          },
           {
             key: "X-Content-Type-Options",
             value: "nosniff",
