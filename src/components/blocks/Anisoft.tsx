745a26d6c6e45a5f11aa0c78b9ccf87b77d878fa
renesauve <rdasauve@gmail.com>
2024-05-24 13:09:08 -0700

    added parse to the text on anisoft

--- src/components/blocks/Anisoft.tsx
+++ src/components/blocks/Anisoft.tsx
@@ -1,5 +1,6 @@
 import type { WpLink } from "@nextwp/core";
 import Image from "next/image";
+import parse from "html-react-parser";
 import Button from "../Button";
 import Edges from "../Edges";
 import FadeInUp from "../FadeInUp";
@@ -41,7 +42,7 @@ export function Anisoft({
               ) : null}
             </div>
             <div className="max-w-[500px] flex flex-col pt-[50px] md:pt-[150px]">
-              {text ? <p>{text}</p> : null}
+              {text ? <p>{parse(text)}</p> : null}
               <div className="pt-[50px] flex wrap gap-[30px] flex-wrap w-auto mr-[50px]">
                 {button?.url ? (
                   <Button href={button.url} variant="large">
